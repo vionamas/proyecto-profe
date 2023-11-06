72da03b7ccc67076f574a46618ee67c4b88c4a64
Sílvia <silviamassana@gmail.com>
2023-11-06 10:48:28 +0100

    config keys

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ export const PORT = process.env.PORT || 3000
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_PORT = process.env.DB_PORT || 3306
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || ''
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'root1234'
 export const DB_NAME = process.env.DB_NAME || 'peliculas'
 
 /* VALORES THEMOVIEDB.ORG */
-export const TMDB_TOKEN_AUTH = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGZlZTM0N2I5MWRhODM5MzJlYThiOWRhYTBlZGVjZSIsInN1YiI6IjYwODJmZTgyMDFiMWNhMDA0MWVjMmUyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FdoxXdJncEQQ24BduFOAz-P3gt6I7pPd6s7vWc8-NrM';
+export const TMDB_TOKEN_AUTH = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMTZhOGEyODIyODE4ZWExNDBhNjM2OWNlZTY3MDdhMyIsInN1YiI6IjY1M2I1N2IyY2M5NjgzMDBhZGU1ZTZhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RK_xgf05bLLZkD4bR4DUQ4AQ55a7LQ9EKf1UiwPlP0g';
 export const TMDB_FETCH_OPTIONS  = {
     method: 'GET',
     headers: {
